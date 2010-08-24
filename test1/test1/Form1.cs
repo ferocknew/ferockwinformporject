c01eb77f97e25e03785c57504f4617db68d13b74
ferock@gmail.com <ferock@gmail.com@516cd15a-ea0f-e908-7dc0-64e867963f47>
2010-08-24 17:35:55 +0000

    最后使用SQLite为数据库

--- test1/test1/Form1.cs
+++ test1/test1/Form1.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Net;
 using System.Net.NetworkInformation;
+using System.Threading;
+using SystemFunction;
 
 namespace test1
 {
@@ -22,8 +25,21 @@ namespace test1
         {
             this.Show();
             this.PingUrlText.Focus();
-        }
 
+            SQLiteConnection conn = new SQLiteConnection();
+            String datasource = @"Data Source=new_sqlite.db3";
+            conn.ConnectionString = datasource;
+            conn.Open();
+
+
+            String _sql = "select name from [admin] where (id=1)";
+            SQLiteCommand cmd = new SQLiteCommand(_sql, conn);
+            SQLiteDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read()) {
+                MessageBox.Show(dr.GetValue(0).ToString());
+            }
+        }
         private void PingButton_Click(object sender, EventArgs e)
         {
             if (PingUrlText.Text == "")
@@ -31,16 +47,29 @@ namespace test1
                 MessageBox.Show("请输入尝试地址！");
                 this.ActiveControl = this.PingUrlText;
             }
-            else {
+            else
+            {
                 //ShowBackLab.Text = PingUrlText.Text;
-                PingReply reply = new Ping().Send("222.32.2.2");
+                PingReply reply = new Ping().Send(PingUrlText.Text.Trim());
                 if (reply.Status == IPStatus.Success)
                 {
-                    ShowBackLab.Text = "Ping successfully.";
+                    MessageBox.Show("Ping successfully.");
                 }
-                else {
-                    ShowBackLab.Text = "Ping failure.";
+                else
+                {
+                    MessageBox.Show("Ping failure.");
                 }
+                //System.Net.WebClient myWebClient = new System.Net.WebClient();
+                //myWebClient.DownloadFileAsync(new Uri("http://dbo.365pub.com/rc/jobs/jobs.rar"), "job.rar");
+                //if (myWebClient.IsBusy)
+                //{
+                //    DownloadBar.Value = 10;
+                //}
+                //else
+                //{
+                //    ShowBackLab.Text = "完成";
+                //}
+                //FileObj.DownFile("http://dbo.365pub.com/rc/jobs/jobs.rar", "job.rar", DownloadBar);
             }
         }
 
---
+++ test1/test1/SystemFunction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SystemFunction
+{
+    class FileObj
+    {
+        ///<summary>
+        ///模块编号：文件下载绑定进度条函数
+        ///作用：用于通过进度条显示文件下载百分比
+        ///作者：兼哲
+        ///编写日期：2010-08-22
+        ///</summary>
+        ///<param   name="URL">下载文件URL</param>
+        ///<returns>说明</returns>
+        public static void DownFile(string URL, string Filename, ProgressBar Prog)
+        {
+            System.Net.HttpWebRequest Myrq = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(URL); System.Net.HttpWebResponse myrp = (System.Net.HttpWebResponse)Myrq.GetResponse(); long totalBytes = myrp.ContentLength; Prog.Maximum = (int)totalBytes; System.IO.Stream st = myrp.GetResponseStream(); System.IO.Stream so = new System.IO.FileStream(Filename, System.IO.FileMode.Create); long totalDownloadedByte = 0; byte[] by = new byte[1024]; int osize = st.Read(by, 0, (int)by.Length); while (osize > 0) { totalDownloadedByte = osize + totalDownloadedByte; Application.DoEvents(); so.Write(by, 0, osize); Prog.Value = (int)totalDownloadedByte; osize = st.Read(by, 0, (int)by.Length); } so.Close(); st.Close();
+        }
+    }
+}
